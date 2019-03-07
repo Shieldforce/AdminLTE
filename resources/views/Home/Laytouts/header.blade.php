e57ba7f88e720937f27b7e560000bcd5e52fe6af
Alexandre Ferreira <shieldforce2@gmail.com>
2019-03-07 10:06:15 -0300

    Adicionando rota de login

--- resources/views/Home/Laytouts/header.blade.php
+++ resources/views/Home/Laytouts/header.blade.php
@@ -2,8 +2,9 @@
     <div class="inner">
         <h3 class="masthead-brand">Shieldforce</h3>
         <nav class="nav nav-masthead justify-content-center">
-            <a class="nav-link active" href="#">Home</a>
+            <a class="nav-link active" href="{{ route('Home.Principal.index') }}">Home</a>
             <a class="nav-link" href="https://shieldforce.com.br">Portal Shieldforce</a>
+            <a class="nav-link active" href="{{ route('login') }}">Entrar</a>
         </nav>
     </div>
 </header>
